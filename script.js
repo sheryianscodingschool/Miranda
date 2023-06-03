b6c6851ad21ccd93a8ad25688c94490be84473a9
sheryiscool <liveclass@sheryians.org>
2023-06-03 16:05:39 +0530

    First Comment

---
+++ script.js
@@ -0,0 +1,23 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl = gsap.timeline()
+
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+tl.to("#page1",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.7
+})
